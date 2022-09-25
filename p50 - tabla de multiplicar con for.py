8fa9624ff0f6141d7f2994104dda04a8a94e1228
edgarviv96 <edgarviverosllamas@gmail.com>
2022-09-24 20:32:44 -0500

    tabla multiplicar

---
+++ p50 - tabla de multiplicar con for.py
@@ -0,0 +1,13 @@
+# tabla de multiplicar con for
+import os
+while(True):
+ os.system('clear')
+ print('Tabla de multiplicar con for \n')
+ t = int(input('Que tabla deseas ? '))
+ n = int(input('Hasta donde la deseas ? '))
+ for i in range(1, n+1):
+  print(f'{t} x {i} = {t*i}')
+ res = input('\n\nDeseas continuar (S/N) ? ').upper()
+ if res=='N':
+  break
+print('Gracias por utilizar este programa')
