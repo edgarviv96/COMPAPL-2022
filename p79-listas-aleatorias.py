cc84b1db67b0da3c567a5cf57b6825ed2b959571
edgarviv96 <edgarviverosllamas@gmail.com>
2022-11-17 13:13:41 -0600

    lista aleatoria

--- p79-listas-aleatorias.py
+++ p79-listas-aleatorias.py
@@ -1,21 +1,27 @@
-# listas con aleatorios
-import random, os
-os.system('clear')
-l1 = []
-l2 = []
-l3 = []
-print("Generando aleatorios ...")
+#generar dos listas de numeros aleatorios
+
+import random
+import os; os.system('cls')
+
+print('Generando lista de numeros aleatorios....')
+
+l1=[]
+l2=[]
+l3=[]
 for n in range(10):
     l1.append(random.randint(1,100))
     l2.append(random.randint(1,100))
-print("\nListas originales:")
-        print(f"Lista 1 : {l1}")
-        print(f"Lista 2 : {l2}")
+
+print('\n listas de numeros originales')
+print(f'lista1  : {l1}')
+print(f'lista 2 : {l2}')
+
 for n in range(10):
-        l1[n]=pow(l1[n],2)
-        l2[n]=pow(l2[n],2)
-        l3.append(l1[n] + l2[n])
-print("\nListas procesadas:")
-print(f"Lista 1 : {l1}")
-print(f"Lista 2 : {l2}")
-print(f"Lista 3 : {l3}")
+    l1[n]= pow(l1[n],2)
+    l2[n]= pow(l2[n],2)
+    l3.append( l1[n] + l2[n])
+
+print('\n listas de numeros elevados')
+print(f'lista 1 : {l1}')
+print(f'lista 2 : {l2}')
+print(f'lista 3 : {l3}')
